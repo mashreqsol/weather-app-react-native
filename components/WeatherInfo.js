9b6baa9013a2dabfab512ce641c0b45e39ebeaad
mashreqsol <47236439+mashreqsol@users.noreply.github.com>
2021-07-13 11:05:16 +0500

    first commit

---
+++ components/WeatherInfo.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { StyleSheet, View, Text, Image } from "react-native";
+import { colors } from "../utils/index";
+
+const { PRIMARY_COLOR, SECONDARY_COLOR } = colors;
+
+export default function WeatherInfo({ currentWeather }) {
+  const {
+    main: { feels_like, temp },
+    weather: [details],
+    name,
+  } = currentWeather;
+
+  const { icon, main } = details;
+  const iconURL = `https://openweathermap.org/img/wn/${icon}@4x.png`;
+  // console.log(iconURL);
+  return (
+    <View style={styles.weatherInfo}>
+      <Text style={styles.weatherInfoTitle}>{name}</Text>
+      <Image style={styles.weatherIcon} source={{ uri: iconURL }} />
+      <Text style={styles.textPrimary}>{temp}°</Text>
+      {/*  <Text>{description}</Text> */}
+      <Text style={styles.textSecondary}>{main}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  weatherInfo: {
+    alignItems: "center",
+  },
+  weatherIcon: {
+    width: 100,
+    height: 100,
+  },
+  weatherInfoTitle: { fontSize: 30, color: PRIMARY_COLOR },
+
+  textPrimary: { fontSize: 40, color: PRIMARY_COLOR },
+  textSecondary: {
+    fontSize: 20,
+    color: SECONDARY_COLOR,
+    fontWeight: "500",
+    marginTop: 10,
+  },
+});
